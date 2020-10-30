d1bcf5e85b035dec6a2dcf6f79e982d60c9ee298
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2020-10-30 17:40:02 -0300

    Add data parsers

--- src/Data/PLY/Parser.hs
+++ src/Data/PLY/Parser.hs
@@ -2,8 +2,10 @@
 module Data.PLY.Parser where
 
 import Control.Applicative
-import Data.Attoparsec.ByteString.Char8
+import Data.Attoparsec.ByteString.Char8 hiding (char)
 import Data.ByteString.Char8 (ByteString, pack)
+import Data.Int (Int8, Int16)
+import Data.Word (Word8, Word16, Word32)
 import Data.PLY.Types
 
 
@@ -32,6 +34,7 @@ property = skipComments *> (scalarProperty <|> listProperty)
                        (skipSpace *> scalarType <* skipSpace) <*>
                        takeLine
 
+-- * Scalar types parser
 scalarType :: Parser ScalarType
 scalarType = choice $
              [ CharT   <$ ("char "   <|> "int8 ")
@@ -43,19 +46,63 @@ scalarType = choice $
              , FloatT  <$ ("float "  <|> "float32 ")
              , DoubleT <$ ("double " <|> "float64 ") ]
 
+-- * Data parser
+dataLine :: [Property] -> Parser [Scalar]
+dataLine = getData []
+  where
+    getData ds [] = pure (reverse ds)
+    getData ds (ScalarProperty t _:ps) = do x <- scalar t
+                                            skipSpace
+                                            getData (x:ds) ps
+    getData _ (ListProperty th td _:_) = do x <- scalar th
+                                            skipSpace
+                                            let c = scalarInt x
+                                            count c (scalar td <* skipSpace)
 
--- * Numeric type parsers
---char
---uchar
+-- | Extract an Int from the Scalar types. Return 0 if float or double.
+scalarInt :: Scalar -> Int
+scalarInt (CharS n)   = fromIntegral n
+scalarInt (UcharS n)  = fromIntegral n
+scalarInt (ShortS n)  = fromIntegral n
+scalarInt (UshortS n) = fromIntegral n
+scalarInt (IntS n)    = n
+scalarInt (UintS n)   = fromIntegral n
+scalarInt _ = 0
+
+-- * Scalar parser
+scalar :: ScalarType -> Parser Scalar
+scalar CharT   = CharS   <$> char
+scalar UcharT  = UcharS  <$> uchar
+scalar ShortT  = ShortS  <$> int16
+scalar UshortT = UshortS <$> uint16
+scalar IntT    = IntS    <$> int
+scalar UintT   = UintS   <$> uint
+scalar FloatT  = FloatS  <$> float
+scalar DoubleT = DoubleS <$> double
+
+-- * Numeric parsers
+char :: Parser Int8
+char = signed decimal
+
+uchar :: Parser Word8
+uchar = decimal
+
+int16 :: Parser Int16
+int16 = signed decimal
+
+uint16 :: Parser Word16
+uint16 = decimal
+
 int :: Parser Int
 int = signed decimal
 
---uint
---int16
---uint16
---float
---double
+uint :: Parser Word32
+uint = decimal
 
+float :: Parser Float
+float = realToFrac <$> double
+
+-- double implemented by attoparsec already
 
 -- * Utility parsers
 skipComments :: Parser ()
