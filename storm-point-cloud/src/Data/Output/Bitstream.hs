2036aeaae5c4b110c99c1742b9ca0f2606670b39
Eduardo Lemos <dudulr10@gmail.com>
2021-05-10 21:52:28 -0300

    Fixed gambiarra -1

--- storm-point-cloud/src/Data/Output/Bitstream.hs
+++ storm-point-cloud/src/Data/Output/Bitstream.hs
@@ -3,6 +3,7 @@ module Data.Output.Bitstream where
 import qualified Data.Structures.Tree as T
 import qualified Data.Structures.Image as I
 import Data.Matrix
+import Data.Maybe
 
 --writeHeader :: (T.BinTree (T.BinTree I.ImageSparse)) -> Header
 
@@ -15,9 +16,8 @@ iRaster2Bin = map f . toList
     where f b = if b then 1 else 0
 
 iRaster2Bin' :: Matrix (Maybe Bool) -> [Int]
-iRaster2Bin' m = filter (>=0) $ toList $ fmap f m
+iRaster2Bin' m = map f $ filter isJust $ toList m
     where f (Just a) = if a then 1 else 0
-          f Nothing = -1
 
 triForce2Bin :: T.BinTree I.ImageSparse -> [Int]
 triForce2Bin tft = iRaster2Bin' (applyMask y yL) ++ iRaster2Bin' (applyMask yL yR)
