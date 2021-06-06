bee5c255491ed4000b5129a02545a9ed0e089c8d
Edil Guimarães <j.edil@ene.unb.br>
2021-06-05 22:41:43 -0300

    Remove disk reading from parser benchmark

--- benchmark/ParserBench.hs
+++ benchmark/ParserBench.hs
@@ -11,15 +11,22 @@ import qualified Data.ByteString as B (readFile)
 
 myConfig = defaultConfig {
   -- Output HTML file
-  reportFile = Just "parser.html"
-  -- Increase time limit to 30 seconds
-  , timeLimit = 30
+  reportFile = Just "parser-bench.html"
+  -- Each benchmark may run for at most 2 minutes.
+  , timeLimit = 120
   }
 
+myEnv = do
+  ddd32 <- B.readFile "test_files/DDD32.ply" -- 17952 points
+  ddd64 <- B.readFile "test_files/DDD64.ply" -- 144554 points
+  return (ddd32, ddd64)
+
 -- Our benchmark harness.
-main = defaultMainWith myConfig [
-  bgroup "ply/parser"
-    [ bench "DDD32.ply"  $ nfIO (parsePLY <$> B.readFile "test_files/DDD32.ply")
-    , bench "DDD64.ply"  $ nfIO (parsePLY <$> B.readFile "test_files/DDD64.ply")
+main = do
+  defaultMainWith myConfig [
+    env myEnv $ \ ~(ddd32, ddd64) ->
+        bgroup "ply/parser"
+        [ bench "DDD32.ply" $ nf parsePLY ddd32
+        , bench "DDD64.ply" $ nf parsePLY ddd64
+        ]
     ]
-  ]
