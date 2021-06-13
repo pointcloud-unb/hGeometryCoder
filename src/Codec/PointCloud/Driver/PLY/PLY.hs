5c085d4545cae9085e7a762a636ae950d10251f9
Otho TK <otho.tk@hotmail.com>
2021-06-13 19:21:23 -0300

    :sparkles: formatPLY from PC function

--- src/Codec/PointCloud/Driver/PLY/PLY.hs
+++ src/Codec/PointCloud/Driver/PLY/PLY.hs
@@ -1,25 +1,50 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Codec.PointCloud.Driver.PLY.PLY
-  ( parsePLY
+  ( parsePLY,
+    formatPLY
   ) where
 
 
 import qualified Data.ByteString as B (ByteString)
-import Data.Attoparsec.ByteString.Char8 hiding (char)
+import Data.Attoparsec.ByteString.Char8
+    ( parse, parseOnly, Result, IResult(Partial, Done, Fail) )
+import qualified Data.ByteString.Char8 as B8 (pack)
 
-import Codec.PointCloud.Driver.PLY.Parser
+import Codec.PointCloud.Driver.PLY.Parser ( elementData, header )
 import Codec.PointCloud.Driver.PLY.Types
+    (Property, Element(Element), Header(Header, hElems),
+     PLY(PLY), DataBlocks, Format (ASCII), Property(ScalarProperty),
+      ScalarType(FloatT), Scalar (FloatS))
 import Control.Monad (forM, join)
+import Codec.PointCloud.Types.PointCloud(PointCloud(PointCloud), pcSize)
+import Codec.PointCloud.Types.Voxel (Voxel(getU, getV, getW))
+import Data.Foldable (Foldable(toList))
 
---
-parsePLY :: B.ByteString -> Either String PLY
+formatPLY :: PointCloud -> PLY
+formatPLY pc = PLY header (formatPLYData pc)
+  where header = Header
+                    ASCII
+                    [Element (B8.pack "vertex") (pcSize pc)
+                                 [ScalarProperty FloatT (B8.pack "x"),
+                                  ScalarProperty FloatT (B8.pack "y"),
+                                  ScalarProperty FloatT (B8.pack "z")]]
+
+formatPLYData :: PointCloud -> DataBlocks
+formatPLYData (PointCloud voxelList _) = fmap formatVoxel (toList voxelList)
+  where formatVoxel voxel = [FloatS ((fromIntegral $ getU voxel)::Float),
+                             FloatS ((fromIntegral $ getV voxel)::Float),
+                             FloatS ((fromIntegral $ getW voxel)::Float)]
+
+
+
+-- parsePLY :: B.ByteString -> Either String PLY
 parsePLY b = do
           resultHeader <- loadHeader b
           case resultHeader of
             (Done r parsedHeader) ->
               let dataParser = forM (hElems parsedHeader) elementData
-              in case parseOnly dataParser r of 
+              in case parseOnly dataParser r of
                 (Left _) -> Left "Invalid data"
                 (Right x) -> return $ PLY parsedHeader (join x)
             _ -> Left "Something crazy happenned"
