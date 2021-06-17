ea62e787740bccc797d7a4ef4eb9e021bca407c3
Edil Guimarães <j.edil@ene.unb.br>
2021-06-16 21:25:07 -0300

    Refactor benchmark

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE DeriveGeneric, DeriveAnyClass, StandaloneDeriving #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass, StandaloneDeriving #-}
 
 module Main where
 
 import Codec.PointCloud.Driver.PLY.Types
-import Codec.PointCloud.Driver.PLY.Parser (parsePLY, parsePLY', unflatPLY)
+import Codec.PointCloud.Driver.PLY.Parser (parsePLY, readPLY, readFlatPLY, unflatPLY)
 import Codec.PointCloud.Driver.PLY.Output (writePLY, putPLY, flatPLY, writeFlatPLY)
 import Data.Either
+import Control.Monad
+import Control.Applicative
 
 import GHC.Generics (Generic)
 import Control.DeepSeq
@@ -16,8 +18,18 @@ import Criterion.Types
 
 import qualified Data.ByteString as B (readFile)
 
---deriving instance Generic DecodeException
---deriving instance NFData DecodeException
+
+assetNames = 
+  ( "dustDense5.ply"  -- 17952 points
+  , "dustDense6.ply"  -- 144554 points
+  , "ricardo9.ply"    -- 214656 points
+  , "ricardo10.ply"   -- 960703 points
+  )
+
+
+emptyPLY = PLY
+  { plyHeader = Header ASCII []
+  , plyData = []}
 
 
 myConfig = defaultConfig {
@@ -27,111 +39,150 @@ myConfig = defaultConfig {
   , timeLimit = 60
   }
 
-plyLoadEnv = do
-  dustDense5 <- B.readFile "assets/dustDense5.ply" -- 17952 points
-  dustDense6 <- B.readFile "assets/dustDense6.ply" -- 144554 points
-  ricardo9 <- B.readFile "assets/ricardo9_frame0000.ply" -- 214656 points
-  ricardo10 <- B.readFile "assets/ricardo10_frame0000.ply" -- 960703 points
-  return (dustDense5, dustDense6, ricardo9, ricardo10)
-
-
-plyLoadParseEnv = do
-  dustDense5 <- parsePLY <$> B.readFile "assets/dustDense5.ply" -- 17952 points
-  dustDense6 <- parsePLY <$> B.readFile "assets/dustDense6.ply" -- 144554 points
-  ricardo9 <- parsePLY <$> B.readFile "assets/ricardo9_frame0000.ply" -- 214656 points
-  ricardo10 <- parsePLY <$> B.readFile "assets/ricardo10_frame0000.ply" -- 960703 points
-  return (dustDense5, dustDense6, ricardo9, ricardo10)
-
-plyLoadFlatEnv = do
-  dustDense5 <- do
-    dustDense5' <- parsePLY <$> B.readFile "assets/dustDense5.ply" -- 17952 points
-    writeFlatPLY "benchmark/results/dustDense5.ply.flat" $ fromRight undefined dustDense5'
-    B.readFile "benchmark/results/dustDense5.ply.flat"
-  
-  dustDense6 <- do
-    dustDense6' <- parsePLY <$> B.readFile "assets/dustDense6.ply" -- 144554 points
-    writeFlatPLY "benchmark/results/dustDense6.ply.flat" $ fromRight undefined dustDense6'
-    B.readFile "benchmark/results/dustDense6.ply.flat"
-
-  ricardo9 <- do
-    ricardo9' <- parsePLY <$> B.readFile "assets/ricardo9_frame0000.ply" -- 214656 points
-    writeFlatPLY "benchmark/results/ricardo9.ply.flat" $ fromRight undefined ricardo9' 
-    B.readFile "benchmark/results/ricardo9.ply.flat"
-
-  ricardo10 <- do
-    ricardo10' <- parsePLY <$> B.readFile "assets/ricardo10_frame0000.ply" -- 960703 points
-    writeFlatPLY "benchmark/results/ricardo10.ply.flat" $ fromRight undefined ricardo10'
-    B.readFile "benchmark/results/ricardo10.ply.flat"
-
-  return (dustDense5, dustDense6, ricardo9, ricardo10)
-
-
 
 
 -- Our benchmark harness.
 main = do
   defaultMainWith myConfig [
-    -- Benchmark parsing.
-    env plyLoadEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/parsePLY"
-        [ bench "dustDense5.ply" $ nf parsePLY dustDense5
-        , bench "dustDense6.ply" $ nf parsePLY  dustDense6
-        , bench "ricardo9.ply" $ nf parsePLY ricardo9
-        , bench "ricardo10.ply" $ nf parsePLY ricardo10
---        , bench "seq-dustDense5.ply" $ nf parsePLY' dustDense5
---        , bench "seq-dustDense6.ply" $ nf parsePLY' dustDense6
---        , bench "seq-ricardo9.ply" $ nf parsePLY' ricardo9
---        , bench "seq-ricardo10.ply" $ nf parsePLY' ricardo10
-        ]
-    -- Benchmark saving PLY to disk
-    , env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/writePLY"
-        [ bench "dustDense5.ply" $ nfIO $
-          writePLY "benchmark/results/dustDense5.ply" $ fromRight undefined dustDense5
-        , bench "dustDense6.ply" $ nfIO $
-          writePLY "benchmark/results/dustDense6.ply" $ fromRight undefined dustDense6
-        , bench "ricardo9_frame0000.ply" $ nfIO $
-          writePLY "benchmark/results/ricardo9_frame0000.ply" $ fromRight undefined ricardo9
-        , bench "ricardo10_frame0000.ply" $ nfIO $
-          writePLY "benchmark/results/ricardo10_frame0000.ply" $ fromRight undefined ricardo10
-        ]
-    -- Benchmark PLY bytestring creation
-    , env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/putPLY"
-        [ bench "dustDense5.ply" $ nf (\x -> putPLY <$> x) dustDense5
-        , bench "dustDense6.ply" $ nf (\x -> putPLY <$> x) dustDense6
-        , bench "ricardo9_frame0000.ply" $ nf (\x -> putPLY <$> x) ricardo9
-        , bench "ricardo10_frame0000.ply" $ nf (\x -> putPLY <$> x) ricardo10
-        ]
-    -- Benchmark saving flat PLY to disk
-    , env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/writeFlatPLY"
-        [ bench "dustDense5.ply" $ nfIO $
-          writeFlatPLY "benchmark/results/dustDense5.ply.flat" $ fromRight undefined dustDense5
-        , bench "dustDense6.ply" $ nfIO $
-          writeFlatPLY "benchmark/results/dustDense6.ply.flat" $ fromRight undefined dustDense6
-        , bench "ricardo9_frame0000.ply" $ nfIO $
-          writeFlatPLY "benchmark/results/ricardo9_frame0000.ply.flat" $ fromRight undefined ricardo9
-        , bench "ricardo10_frame0000.ply" $ nfIO $
-          writeFlatPLY "benchmark/results/ricardo10_frame0000.ply.flat" $ fromRight undefined ricardo10
-        ]
-    -- Benchmark PLY flating
-    , env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/flatPLY"
-        [ bench "dustDense5.ply" $ nf (\x -> flatPLY <$> x) dustDense5
-        , bench "dustDense6.ply" $ nf (\x -> flatPLY <$> x) dustDense6
-        , bench "ricardo9_frame0000.ply" $ nf (\x -> flatPLY <$> x) ricardo9
-        , bench "ricardo10_frame0000.ply" $ nf (\x -> flatPLY <$> x) ricardo10
-        ]
-    -- Benchmark load flat PLY
-    , env plyLoadFlatEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
-        bgroup "ply/unflatPLY"
-        [ bench "dustDense5.ply" $ nf (fromRight undefined . unflatPLY) dustDense5
-        , bench "dustDense6.ply" $ nf (fromRight undefined . unflatPLY) dustDense6
-        , bench "ricardo9_frame0000.ply" $ nf (fromRight undefined . unflatPLY) ricardo9
-        , bench "ricardo10_frame0000.ply" $ nf (fromRight undefined . unflatPLY)  ricardo10
-        ]
-
+    bgroup "PLY" [
+      bgroup "Input" [ parsePLYBench
+                     , readPLYBench
+                     , unflatPLYBench
+                     , readFlatPLYBench
+                     ]
+      , bgroup "Output" [ putPLYBench
+                        , writePLYBench
+                        , flatPLYBench
+                        , writeFlatPLYBench
+                        ]
+      ]
     ]
-    
+
+
+
+-- PLY input benchmarks --
+plyLoadEnv = do
+ dustDense5 <- B.readFile "assets/dustDense5.ply"
+ dustDense6 <- B.readFile "assets/dustDense6.ply"
+ ricardo9 <- B.readFile "assets/ricardo9.ply"
+ ricardo10 <- B.readFile "assets/ricardo10.ply"
+ return (dustDense5, dustDense6, ricardo9, ricardo10)
+
+parsePLYBench = env plyLoadEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+  bgroup "parsePLY"
+  [ bench "dustDense5.ply" $ nf parsePLY dustDense5
+--  , bench "dustDense6.ply" $ nf parsePLY dustDense6
+--  , bench "ricardo9.ply"   $ nf parsePLY ricardo9
+  , bench "ricardo10.ply"  $ nf parsePLY ricardo10
+  ]
+
+readPLYBench = 
+  bgroup "readPLY"
+  [ bench "dustDense5.ply" $ nfAppIO readPLY "assets/dustDense5.ply"
+--  , bench "dustDense6.ply" $ nfAppIO readPLY "assets/dustDense6.ply"
+--  , bench "ricardo9.ply"   $ nfAppIO readPLY "assets/ricardo9.ply"
+  , bench "ricardo10.ply"  $ nfAppIO readPLY "assets/ricardo10.ply"
+  ]
+
+
+
+plyLoadFlatEnv = do
+  dustDense5 <- do
+    dustDense5' <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/dustDense5.ply" 
+    writeFlatPLY "benchmark/results/dustDense5.ply.flat" dustDense5'
+    B.readFile "benchmark/results/dustDense5.ply.flat"
+  
+  dustDense6 <- do
+    dustDense6' <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/dustDense6.ply"
+    writeFlatPLY "benchmark/results/dustDense6.ply.flat" dustDense6'
+    B.readFile "benchmark/results/dustDense6.ply.flat"
+
+  ricardo9 <- do
+    ricardo9' <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/ricardo9.ply"
+    writeFlatPLY "benchmark/results/ricardo9.ply.flat" ricardo9' 
+    B.readFile "benchmark/results/ricardo9.ply.flat"
+
+  ricardo10 <- do
+    ricardo10' <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/ricardo10.ply"
+    writeFlatPLY "benchmark/results/ricardo10.ply.flat" ricardo10'
+    B.readFile "benchmark/results/ricardo10.ply.flat"
+
+  return (dustDense5, dustDense6, ricardo9, ricardo10)
+
+
+unflatPLYBench =
+  let benchFunction = (fromRight emptyPLY . unflatPLY)
+  in
+    env plyLoadFlatEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "unflatPLY"
+    [ bench "dustDense5.ply" $ nf benchFunction dustDense5
+    -- , bench "dustDense6.ply" $ nf benchFunction dustDense6
+    -- , bench "ricardo9.ply" $ nf benchFunction ricardo9
+    , bench "ricardo10.ply" $ nf benchFunction  ricardo10
+    ]
+
+readFlatPLYBench =
+  let benchFunction file = fromRight emptyPLY <$> readFlatPLY file
+  in
+    env plyLoadFlatEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "readFlatPLY"
+    [ bench "dustDense5.ply" $ nfAppIO benchFunction "benchmark/results/dustDense5.ply.flat"
+    -- , bench "dustDense6.ply" $ nfAppIO benchFunction "benchmark/results/dustDense6.ply.flat"
+    -- , bench "ricardo9.ply"   $ nfAppIO benchFunction "benchmark/results/ricardo9.ply.flat"
+    , bench "ricardo10.ply"  $ nfAppIO benchFunction "benchmark/results/ricardo10.ply.flat"
+    ]
+
+
+-- PLY output benchmarks -- 
+plyLoadParseEnv = do
+  dustDense5 <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/dustDense5.ply"
+  dustDense6 <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/dustDense6.ply"
+  ricardo9 <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/ricardo9.ply"
+  ricardo10 <- fromRight emptyPLY . parsePLY <$> B.readFile "assets/ricardo10.ply"
+  return (dustDense5, dustDense6, ricardo9, ricardo10)
+
+putPLYBench =
+  let benchFunction = putPLY
+  in
+    env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "putPLY"
+    [ bench "dustDense5.ply" $ nf benchFunction dustDense5
+    -- , bench "dustDense6.ply" $ nf benchFunction dustDense6
+    -- , bench "ricardo9.ply"   $ nf benchFunction ricardo9
+    , bench "ricardo10.ply"  $ nf benchFunction ricardo10
+    ]
+
+writePLYBench =
+  let benchFunction = writePLY
+  in
+    env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "writePLY"
+    [ bench "dustDense5.ply" $ nfAppIO (benchFunction "benchmark/results/dustDense5.ply") dustDense5
+      -- , bench "dustDense6.ply" $ nfAppIO (benchFunction "benchmark/results/dustDens6.ply") dustDense6
+      -- , bench "ricardo9.ply"   $ nfAppIO (benchFunction "benchmark/results/ricardo9.ply") ricardo9
+    , bench "ricardo10.ply"  $ nfAppIO (benchFunction "benchmark/results/ricardo10.ply") ricardo10
+    ]
+
+flatPLYBench =
+  let benchFunction = flatPLY
+  in
+    env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "flatPLY"
+    [ bench "dustDense5.ply" $ nf benchFunction dustDense5
+      -- , bench "dustDense6.ply" $ nf benchFunction dustDense6
+      -- , bench "ricardo9.ply"   $ nfbenchFunction ricardo9
+    , bench "ricardo10.ply"  $ nf benchFunction ricardo10
+    ]
+
+writeFlatPLYBench =
+  let benchFunction = writeFlatPLY
+  in
+    env plyLoadParseEnv $ \ ~(dustDense5, dustDense6, ricardo9, ricardo10) ->
+    bgroup "writeFlatPLY"
+    [ bench "dustDense5.ply" $ nfAppIO (benchFunction "benchmark/results/dustDense5.ply") dustDense5
+      -- , bench "dustDense6.ply" $ nfAppIO (benchFunction "benchmark/results/dustDens6.ply") dustDense6
+      -- , bench "ricardo9.ply"   $ nfAppIO (benchFunction "benchmark/results/ricardo9.ply") ricardo9
+    , bench "ricardo10.ply"  $ nfAppIO (benchFunction "benchmark/results/ricardo10.ply") ricardo10
+    ]
+
 
