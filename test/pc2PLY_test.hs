ebdcb288c78744b961cd87c50fe852edaef8191e
Otho TK <otho.tk@hotmail.com>
2021-06-24 19:41:22 -0300

    :white_check_mark: pc2PLY tests

--- test/pc2PLY_test.hs
+++ test/pc2PLY_test.hs
@@ -21,9 +21,9 @@ import Data.Foldable (Foldable(toList))
 import qualified Data.Set as S
 
 main :: IO ()
-setPc = S.fromList [Voxel 1 2 3]
-pc = PointCloud setPc 1
-plyResult = PLY header content
+setPc1 = S.fromList [Voxel 1 2 3]
+pc1 = PointCloud setPc1 1
+plyResult1 = PLY header content
   where
     header =
       Header
@@ -37,11 +37,31 @@ plyResult = PLY header content
         ]
     content =  [[FloatS 1.0, FloatS 2.0, FloatS 3.0]]
 
+setPc2 = S.fromList []
+pc2 = PointCloud setPc2 2
+plyResult2 = PLY header content
+  where
+    header =
+      Header
+        ASCII
+        [ Element
+            (B8.pack "vertex") 0
+            [ ScalarProperty FloatT (B8.pack "x")
+            , ScalarProperty FloatT (B8.pack "y")
+            , ScalarProperty FloatT (B8.pack "z")
+            ]
+        ]
+    content =  []
+
 main = hspec $ do
   describe "pc2PLY" $ do
     context "Conversão de uma pc padrão" $ do
-      it "match pc2PLY (PointCloud (Set {Voxel 1 2 3}) 4) should be {plyHeader = Header \
+      it "match pc2PLY (PointCloud (Set {Voxel 1 2 3}) 1) should be\n {plyHeader = Header \
             \...plyData = [[FloatS 1.0,FloatS 2.0,FloatS 3.0]]}" $
-        pc2PLY pc `shouldBe` plyResult
+        pc2PLY pc1 `shouldBe` plyResult1
+    context "Conversão de uma pc vazia" $ do
+      it "match pc2PLY (PointCloud (Set {Voxel 1 2 15, Voxel 1 3 1}) 1) should be\n {plyHeader = Header \
+            \...(B8.pack \"vertex\") 2 \nplyData = [}" $
+        pc2PLY pc2 `shouldBe` plyResult2
 
     
