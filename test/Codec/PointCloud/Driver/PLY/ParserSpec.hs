610a753b364c55f910a07dfae4bb819fb0f1fb9c
Edil Guimarães <j.edil@ene.unb.br>
2021-06-27 16:36:17 -0300

    Code reorganization

--- test/Codec/PointCloud/Driver/PLY/ParserSpec.hs
+++ test/Codec/PointCloud/Driver/PLY/ParserSpec.hs
@@ -13,25 +13,36 @@ import Test.Hspec.Attoparsec
 spec :: Spec
 spec = do
   describe "Parser" $ do
-    describe "formatLine" $ do
-      describe "ASCII" $ do
-        it "Proper line" $
-          ("format ascii 1.0" :: B.ByteString) ~> format `shouldParse` ASCII
-        it "Spaces in the middle of line" $
-          ("format   ascii   1.0   " :: B.ByteString) ~> format `shouldParse` ASCII
-        it "Tabs in the middle of line" $
-          ("format\tascii\t1.0" :: B.ByteString) ~> format `shouldParse` ASCII
-      describe "Little_Endian" $ do
-        it "Proper line" $
-          ("format binary_little_endian 1.0" :: B.ByteString) ~> format `shouldParse` BinaryLE
-        it "Spaces in the middle of line" $
-          ("format   binary_little_endian   1.0   " :: B.ByteString) ~> format `shouldParse` BinaryLE
-        it "Tabs in the middle of line" $
-          ("format\tbinary_little_endian\t1.0" :: B.ByteString) ~> format `shouldParse` BinaryLE
-      describe "Big_Endian" $ do
-        it "Proper line" $
-          ("format binary_big_endian 1.0" :: B.ByteString) ~> format `shouldParse` BinaryBE
-        it "Spaces in the middle of line" $
-          ("format   binary_big_endian   1.0   " :: B.ByteString) ~> format `shouldParse` BinaryBE
-        it "Tabs in the middle of line" $
-          ("format\tbinary_big_endian\t1.0" :: B.ByteString) ~> format `shouldParse` BinaryBE
+    formatSpec
+    propertySpec
+
+formatSpec = do
+  describe "formatLine" $ do
+    describe "ASCII" $ do
+      it "Proper line" $
+        ("format ascii 1.0" :: B.ByteString) ~> format `shouldParse` ASCII
+      it "Spaces in the middle of line" $
+        ("format   ascii   1.0   " :: B.ByteString) ~> format `shouldParse` ASCII
+      it "Tabs in the middle of line" $
+        ("format\tascii\t1.0" :: B.ByteString) ~> format `shouldParse` ASCII
+    describe "Little_Endian" $ do
+      it "Proper line" $
+        ("format binary_little_endian 1.0" :: B.ByteString) ~> format `shouldParse` BinaryLE
+      it "Spaces in the middle of line" $
+        ("format   binary_little_endian   1.0   " :: B.ByteString) ~> format `shouldParse` BinaryLE
+      it "Tabs in the middle of line" $
+        ("format\tbinary_little_endian\t1.0" :: B.ByteString) ~> format `shouldParse` BinaryLE
+    describe "Big_Endian" $ do
+      it "Proper line" $
+        ("format binary_big_endian 1.0" :: B.ByteString) ~> format `shouldParse` BinaryBE
+      it "Spaces in the middle of line" $
+        ("format   binary_big_endian   1.0   " :: B.ByteString) ~> format `shouldParse` BinaryBE
+      it "Tabs in the middle of line" $
+        ("format\tbinary_big_endian\t1.0" :: B.ByteString) ~> format `shouldParse` BinaryBE
+
+propertySpec = do
+  describe "property" $ do
+    describe "ScalarProperty" $ do
+      it "" $ pending
+    describe "ScalarProperty" $ do
+      it "" $ pending
